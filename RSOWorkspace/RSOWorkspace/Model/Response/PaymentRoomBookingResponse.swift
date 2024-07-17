f010f26db96ebe1be1603e526c31382625b76438
Pradip Vijay Deore <pradip@creativegalileo.com>
2024-07-17 22:41:55 +0530

    booking saved

--- RSOWorkspace/RSOWorkspace/Model/Response/PaymentRoomBookingResponse.swift
+++ RSOWorkspace/RSOWorkspace/Model/Response/PaymentRoomBookingResponse.swift
@@ -6,8 +6,36 @@
 //
 
 import Foundation
+enum Status: Codable {
+    case bool(Bool)
+    case string(String)
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if let boolValue = try? container.decode(Bool.self) {
+            self = .bool(boolValue)
+            return
+        }
+        if let stringValue = try? container.decode(String.self) {
+            self = .string(stringValue)
+            return
+        }
+        throw DecodingError.typeMismatch(Status.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Expected Bool or String"))
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        switch self {
+        case .bool(let boolValue):
+            try container.encode(boolValue)
+        case .string(let stringValue):
+            try container.encode(stringValue)
+        }
+    }
+}
+
 
 struct PaymentRoomBookingResponse: Codable {
-    let status: Bool
+    let status: Status
     let message: String
 }
