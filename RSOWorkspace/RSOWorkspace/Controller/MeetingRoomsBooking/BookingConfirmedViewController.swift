c1219c1e63935a7be33bade57d0dd68e51c6c7a7
Pradip Vijay Deore <pradip@creativegalileo.com>
2024-07-17 23:06:12 +0530

    edit action added

--- RSOWorkspace/RSOWorkspace/Controller/MeetingRoomsBooking/BookingConfirmedViewController.swift
+++ RSOWorkspace/RSOWorkspace/Controller/MeetingRoomsBooking/BookingConfirmedViewController.swift
@@ -243,8 +243,8 @@ extension BookingConfirmedViewController {
 }
 extension BookingConfirmedViewController:ButtonEditTableViewCellDelegate{
     func navigateToBookingDetails() {
-        self.dismiss(animated:true)
-
+        //self.dismiss(animated:true)
+      self.navigationController?.popViewController(animated: true)
     }
 }
 extension BookingConfirmedViewController:ConfirmAndProceedToPayementTableViewCellDelegate{
