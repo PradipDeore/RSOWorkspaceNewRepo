a60072fca876e4c09e16b66aaea63b10c08829d3
Bharati <wagh.bharati9213@gmail.com>
2024-07-25 23:12:48 +0530

    minor issues fix

--- RSOWorkspace/RSOWorkspace/Controller/Office Booking/BookAnOfficeViewController.swift
+++ RSOWorkspace/RSOWorkspace/Controller/Office Booking/BookAnOfficeViewController.swift
@@ -27,66 +27,122 @@ class BookAnOfficeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         coordinator?.hideBackButton(isHidden:false)
         coordinator?.setTitle(title: "Book an Office")
         setButtonsANDTextFields()
-        
     }
+    
+    @IBAction func btnDropDown(_ sender: Any) {
+        let alertController = UIAlertController(title: "Select Option", message: nil, preferredStyle: .actionSheet)
+        let privateOfficeAction = UIAlertAction(title: "Private Office", style: .default) { [weak self] _ in
+            self?.txtInterestedIn.text = "Private Office"
+        }
+        let officeSuiteAction = UIAlertAction(title: "Office Suite", style: .default) { [weak self] _ in
+            self?.txtInterestedIn.text = "Office Suite"
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alertController.addAction(privateOfficeAction)
+        alertController.addAction(officeSuiteAction)
+        alertController.addAction(cancelAction)
+        
+        // For iPad support
+        if let popoverController = alertController.popoverPresentationController {
+            popoverController.sourceView = sender as? UIView
+            popoverController.sourceRect = (sender as AnyObject).bounds
+        }
+        
+        present(alertController, animated: true, completion: nil)
+    }
+
+    @IBAction func btnIncrementSeatsCount(_ sender: Any) {
+        if let currentCount = Int(txtNoOfSeats.text ?? "0") {
+                    txtNoOfSeats.text = "\(currentCount + 1)"
+                } else {
+                    txtNoOfSeats.text = "1"
+                }
+    }
+    
+    @IBAction func btnDecrementSeatsCount(_ sender: Any) {
+        if let currentCount = Int(txtNoOfSeats.text ?? "0"), currentCount > 0 {
+                   txtNoOfSeats.text = "\(currentCount - 1)"
+               } else {
+                   txtNoOfSeats.text = "0"
+               }
+    }
+    
     func setButtonsANDTextFields(){
-        self.btnMinus.layer.cornerRadius = btnMinus.bounds.height / 2
-        self.btnPlus.layer.cornerRadius = btnPlus.bounds.height / 2
-        txtprovideDetails.addPlaceholder(text: "Please provide any details")
-        txtInterestedIn.setUpTextFieldView(rightImageName:"arrowdown")
-        provideDetailsView.setCornerRadiusForView()
-        
-    }
-    func bookOffice(requestModel: BookOfficeRequestModel) {
-        
-        APIManager.shared.request(
-            modelType: OfficeBookingResponseModel.self,
-            type: MyBookingEndPoint.officeBooking(requestModel: requestModel)) { [weak self] response in
-                
-                guard let self = self else { return }                
-                switch response {
-                case .success(let responseData):
-                    // Handle successful response with bookings
-                    self.officeBookingResonseData = responseData.data
-                    DispatchQueue.main.async {
-                        RSOToastView.shared.show("Booking Saved For Office", duration: 2.0, position: .center)
-                    }
-                    self.eventHandler?(.dataLoaded)
-                case .failure(let error):
-                    self.eventHandler?(.error(error))
-                    DispatchQueue.main.async {
-                        RSOToastView.shared.show("\(error.localizedDescription)", duration: 2.0, position: .center)
-                    }
+    self.btnMinus.layer.cornerRadius = btnMinus.bounds.height / 2
+    self.btnPlus.layer.cornerRadius = btnPlus.bounds.height / 2
+    txtprovideDetails.addPlaceholder(text: "Please provide any details")
+    txtInterestedIn.setUpTextFieldView(rightImageName:"arrowdown")
+    provideDetailsView.setCornerRadiusForView()
+    
+}
+func bookOffice(requestModel: BookOfficeRequestModel) {
+    
+    APIManager.shared.request(
+        modelType: OfficeBookingResponseModel.self,
+        type: MyBookingEndPoint.officeBooking(requestModel: requestModel)) { [weak self] response in
+            
+            guard let self = self else { return }
+            switch response {
+            case .success(let responseData):
+                // Handle successful response with bookings
+                self.officeBookingResonseData = responseData.data
+                DispatchQueue.main.async {
+                    RSOToastView.shared.show("Booking Saved For Office", duration: 2.0, position: .center)
+                }
+                self.eventHandler?(.dataLoaded)
+            case .failure(let error):
+                self.eventHandler?(.error(error))
+                DispatchQueue.main.async {
+                    RSOToastView.shared.show("\(error.localizedDescription)", duration: 2.0, position: .center)
                 }
             }
-    }
-   @IBAction func btnSubmitTappedAction(_ sender: Any) {
-        guard let name = txtName.text, !name.isEmpty,
-              let email = txtEmail.text, !email.isEmpty,
-              let phone = txtPhone.text, !phone.isEmpty,
-              let interestedIn = txtInterestedIn.text, !interestedIn.isEmpty,
-              let noOfSeatsText = txtNoOfSeats.text, let noOfSeats = Int(noOfSeatsText),
-              let provideDetails = txtprovideDetails.text else {
-            // Show an alert or toast to inform the user that all fields are required
-            RSOToastView.shared.show("Please fill in all fields", duration: 2.0, position: .center)
+        }
+}
+@IBAction func btnSubmitTappedAction(_ sender: Any) {
+    guard let name = txtName.text, !name.isEmpty else {
+            RSOToastView.shared.show("Please fill in your name", duration: 2.0, position: .center)
             return
         }
         
-//        let requestModel = BookOfficeRequestModel(
-//            name: name,
-//            email: email,
-//            phone: phone,
-//            interestedIn: interestedIn,
-//            noOfSeats: noOfSeats,
-//            provideDetails: provideDetails
-//        )
-//        bookOffice(requestModel: requestModel)
-   }
-    
+        guard let email = txtEmail.text, !email.isEmpty else {
+            RSOToastView.shared.show("Please fill in your email", duration: 2.0, position: .center)
+            return
+        }
+        
+        guard let phone = txtPhone.text, !phone.isEmpty else {
+            RSOToastView.shared.show("Please fill in your phone number", duration: 2.0, position: .center)
+            return
+        }
+        
+        guard let interestedIn = txtInterestedIn.text, !interestedIn.isEmpty else {
+            RSOToastView.shared.show("Please select your interest", duration: 2.0, position: .center)
+            return
+        }
+        
+        guard let noOfSeatsText = txtNoOfSeats.text, let noOfSeats = Int(noOfSeatsText) else {
+            RSOToastView.shared.show("Please provide the number of seats", duration: 2.0, position: .center)
+            return
+        }
+        
+        guard let provideDetails = txtprovideDetails.text, !provideDetails.isEmpty else {
+            RSOToastView.shared.show("Please provide any details", duration: 2.0, position: .center)
+            return
+        }
+    //        let requestModel = BookOfficeRequestModel(
+    //            name: name,
+    //            email: email,
+    //            phone: phone,
+    //            interestedIn: interestedIn,
+    //            noOfSeats: noOfSeats,
+    //            provideDetails: provideDetails
+    //        )
+    //        bookOffice(requestModel: requestModel)
+}
+
 }
 
 extension BookAnOfficeViewController {
